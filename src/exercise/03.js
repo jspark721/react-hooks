09d8cc760b432b8ba636dc68716c12d76872dad1
jspark721 <jspark721@gmail.com>
2021-08-02 18:03:36 -0700

    finish exercise 4

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -4,11 +4,15 @@
 import * as React from 'react'
 
 function Name() {
-  const [name, setName] = React.useState('');
-  return(
+  const [name, setName] = React.useState('')
+  return (
     <div>
-      <label htmlFor='name'>Name: </label>
-      <input id='name' value={name} onChange={event = setName(event.target.value)} />
+      <label htmlFor="name">Name: </label>
+      <input
+        id="name"
+        value={name}
+        onChange={event => setName(event.target.value)}
+      />
     </div>
   )
 }
@@ -17,11 +21,7 @@ function FavoriteAnimal({animal, onAnimalChange}) {
   return (
     <div>
       <label htmlFor="animal">Favorite Animal: </label>
-      <input
-        id="animal"
-        value={animal}
-        onChange={onAnimalChange}
-      />
+      <input id="animal" value={animal} onChange={onAnimalChange} />
     </div>
   )
 }
@@ -31,14 +31,16 @@ function Display({animal}) {
   return <div>{`Your favorite animal is: ${animal}!`}</div>
 }
 
-
 function App() {
-  const [animal, setAnimal] = React.useState('');
+  const [animal, setAnimal] = React.useState('')
   return (
     <form>
       <Name />
-      <FavoriteAnimal animal={animal} onAnimalChange={e => setAnimal(e.target.value)} />
-      <Display animal={animal}/>
+      <FavoriteAnimal
+        animal={animal}
+        onAnimalChange={e => setAnimal(e.target.value)}
+      />
+      <Display animal={animal} />
     </form>
   )
 }
